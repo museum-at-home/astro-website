0656d5042a0c228f4ba1edc140951fc1b2cebb63
museum-at-home <giannamarie209@gmmail.com>
2026-08-31 13:09:11 -0400

    Adding the JSON data table to create a searchable array for the user to find the museum they want.

---
+++ src/types/museum.ts
@@ -0,0 +1,12 @@
+export interface Museum {
+  id: number;
+  name: string;
+  location: string;
+  type: 'Art' | 'Science' | 'History';
+  description: string;
+  website: string;
+  has3DTour: boolean;
+  hasAudioTour: boolean;
+  rating: 1 | 2 | 3 | 4 | 5;
+  imageUrl: string;
+}
